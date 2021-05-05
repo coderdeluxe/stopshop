41b1df24ffc46652c08e6daf3cd169424b121e90
Raymond <coderdeluxe@gmail.com>
2021-05-05 22:25:18 +0800

    Added Shop UI

--- Core/Specification/ProductSpecParams.cs
+++ Core/Specification/ProductSpecParams.cs
@@ -4,7 +4,7 @@ namespace Core.Specification
     {
         private const int MaxPageSize = 50;
         public int PageIndex { get; set; } = 1;
-        private int _pageSize = 5;
+        private int _pageSize = 6;
         public int PageSize 
         {
             get => _pageSize;
