96ea2944c893ce531240cc793602e92d442114b8
Raymond <coderdeluxe@gmail.com>
2021-05-23 09:11:29 +0800

    Added API validation

--- API/Dto/AddressDto.cs
+++ API/Dto/AddressDto.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dto
 {
     public class AddressDto
     {
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string Barangay { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Province { get; set; }
     }
 }
--- API/Dto/RegisterDto.cs
+++ API/Dto/RegisterDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dto
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"((?=.*\d)(?=.*[A-Z]).{8,50})", ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 Number, 1 Non Alphanumeric and at least 8 characters")]
         public string Password { get; set; }
     }
 }
